24f96c92f17f08194f5434673b2d1a4578450e92
Juan David Valencia <juandavalg@gmail.com>
2023-07-17 19:01:48 +0200

    Vikings

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,37 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+}
 
 // Saxon
 class Saxon {}
 
 // War
-class War {}
+class War {
+
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }   
+}
+
